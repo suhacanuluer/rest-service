2749bee5fc4836fb32171e85fd87349c5c750d4c
suhacanuluer <sculuer@gmail.com>
2022-04-11 01:36:54 +0300

    added parameter

--- src/main/java/com/example/restservice/HelloWorldController.java
+++ src/main/java/com/example/restservice/HelloWorldController.java
@@ -1,15 +1,17 @@
 package com.example.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    private static final String message = "Hello World!";
+    private static final String template = "Hello, %s!";
 
+    // /helloworld?message='blabla'
     @GetMapping("/helloworld")
-    public HelloWorld helloWorld() {
-        return new HelloWorld(message);
+    public HelloWorld helloWorld(@RequestParam(value = "message", defaultValue = "World") String message) {
+        return new HelloWorld(String.format(template, message));
     }
 }
